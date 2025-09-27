ecd5277faaa19d3feec6d0d88bc26bbf67c4d680
SapeiIdw <sapeiidw@gmail.com>
2025-09-27 16:14:47 +0800

    add : shadcn

---
+++ src/components/Provider.tsx
@@ -0,0 +1,17 @@
+// providers/query-client-provider.tsx
+"use client";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export function Provider({ children }: { children: ReactNode }) {
+  // ensure stable instance across HMR
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <ClerkProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ClerkProvider>
+  );
+}
