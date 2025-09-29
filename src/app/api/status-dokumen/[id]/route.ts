f59f52373e6a9185b1e4c608c5590bfbe3b36cbe
Sapeiidw <sapeiidw@gmail.com>
2025-09-29 22:33:34 +0800

    fix ;

---
+++ src/app/api/status-dokumen/[id]/route.ts
@@ -0,0 +1,77 @@
+// /app/api/kenaikan-pangkat/[id]/route.ts
+import { status_dokumen } from "@/db/schema";
+import { db } from "@/lib/db";
+import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = parseInt(params.id);
+  const body = await req.json();
+
+  if (isNaN(id)) {
+    return new Response(JSON.stringify({ error: "Invalid ID" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const updated = await db
+    .update(status_dokumen)
+    .set(body)
+    .where(eq(status_dokumen.id, id))
+    .returning();
+
+  if (updated.length === 0) {
+    return new Response(JSON.stringify({ error: "Data not found" }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return new Response(JSON.stringify({ message: "Berhasil diperbarui" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = parseInt(params.id);
+
+    if (isNaN(id)) {
+      return new Response(JSON.stringify({ error: "Invalid ID" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const deleted = await db
+      .delete(status_dokumen)
+      .where(eq(status_dokumen.id, id))
+      .returning();
+
+    if (deleted.length === 0) {
+      return new Response(JSON.stringify({ error: "Data not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response(JSON.stringify({ message: "Berhasil dihapus" }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Delete Error:", error);
+    return new Response(JSON.stringify({ error: "Server error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
