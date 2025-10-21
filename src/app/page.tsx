a1e0349157457b73822fea5a4e3a467ad88044df
Sapeiidw <sapeiidw@gmail.com>
2025-10-21 21:45:45 +0800

    fix : dasboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,34 @@
 import BarChartCustom from "@/components/chart/BarChart";
 import LineChartCustom from "@/components/chart/LineChart";
 import PieChartCustom from "@/components/chart/PieChart";
-import { kenaikan_pangkat, status_dokumen } from "@/db/schema";
+import { kenaikan_pangkat, status_dokumen_wajib } from "@/db/schema";
 import { db } from "@/lib/db";
 import { sql } from "drizzle-orm";
 
+const dataKenaikanPangkat = await db
+  .select({
+    label: sql<string>`TRIM(TO_CHAR(DATE '2025-09-01', 'Month'))`.as("label"),
+    value: sql<number>`SUM(kenaikan_pangkat.value)`.as("value"),
+  })
+  .from(kenaikan_pangkat)
+  .groupBy(kenaikan_pangkat.periode)
+  .orderBy(kenaikan_pangkat.periode);
+
+const dataStatusDokumen = await db
+  .select({
+    label: sql<string>`TRIM(TO_CHAR(DATE '2025-09-01', 'Month'))`.as("label"),
+    berhasil: sql<number>`SUM(status_dokumen_wajib.berhasil)`.as("berhasil"),
+    tidak_berhasil: sql<number>`SUM(status_dokumen_wajib.tidak_berhasil)`.as(
+      "tidak_berhasil"
+    ),
+  })
+  .from(status_dokumen_wajib)
+  .groupBy(status_dokumen_wajib.periode)
+  .orderBy(status_dokumen_wajib.periode);
+
 export default async function Page() {
-  // Get the Backend API User object when you need access to the user's information
-
-  const dataKenaikanPangkat = await db
-    .select({
-      label: kenaikan_pangkat.bulan,
-      value: sql<number>`SUM(kenaikan_pangkat.value)`.as("value"),
-    })
-    .from(kenaikan_pangkat)
-    .groupBy(kenaikan_pangkat.tahun, kenaikan_pangkat.bulan)
-    .orderBy(
-      sql`CASE 
-      WHEN ${kenaikan_pangkat.bulan} = 'Januari' THEN 1
-      WHEN ${kenaikan_pangkat.bulan} = 'Februari' THEN 2
-      WHEN ${kenaikan_pangkat.bulan} = 'Maret' THEN 3
-      WHEN ${kenaikan_pangkat.bulan} = 'April' THEN 4
-      WHEN ${kenaikan_pangkat.bulan} = 'Mei' THEN 5
-      WHEN ${kenaikan_pangkat.bulan} = 'Juni' THEN 6
-      WHEN ${kenaikan_pangkat.bulan} = 'Juli' THEN 7
-      WHEN ${kenaikan_pangkat.bulan} = 'Agustus' THEN 8
-      WHEN ${kenaikan_pangkat.bulan} = 'September' THEN 9
-      WHEN ${kenaikan_pangkat.bulan} = 'Oktober' THEN 10
-      WHEN ${kenaikan_pangkat.bulan} = 'November' THEN 11
-      WHEN ${kenaikan_pangkat.bulan} = 'Desember' THEN 12
-      ELSE 13
-    END`
-    );
-
-  const dataStatusDokumen = await db
-    .select({
-      label: status_dokumen.bulan,
-      berhasil: sql<number>`SUM(status_dokumen.berhasil)`.as("berhasil"),
-      tidak_berhasil: sql<number>`SUM(status_dokumen.tidak_berhasil)`.as(
-        "tidak_berhasil"
-      ),
-    })
-    .from(status_dokumen)
-    .groupBy(status_dokumen.tahun, status_dokumen.bulan)
-    .orderBy(
-      sql`CASE 
-      WHEN ${status_dokumen.bulan} = 'Januari' THEN 1
-      WHEN ${status_dokumen.bulan} = 'Februari' THEN 2
-      WHEN ${status_dokumen.bulan} = 'Maret' THEN 3
-      WHEN ${status_dokumen.bulan} = 'April' THEN 4
-      WHEN ${status_dokumen.bulan} = 'Mei' THEN 5
-      WHEN ${status_dokumen.bulan} = 'Juni' THEN 6
-      WHEN ${status_dokumen.bulan} = 'Juli' THEN 7
-      WHEN ${status_dokumen.bulan} = 'Agustus' THEN 8
-      WHEN ${status_dokumen.bulan} = 'September' THEN 9
-      WHEN ${status_dokumen.bulan} = 'Oktober' THEN 10
-      WHEN ${status_dokumen.bulan} = 'November' THEN 11
-      WHEN ${status_dokumen.bulan} = 'Desember' THEN 12
-      ELSE 13
-    END`
-    );
-  // Use `user` to render user details or create UI elements
   return (
     <>
-      {/* <div className="flex justify-between items-center px-8 py-2 gap-4 sticky top-0 w-full bg-white shadow">
-        <h1 className="text-xl">MAMANK US</h1>
-        <UserButton showName />
-      </div> */}
       <div className="w-full h-96 col-span-8 bg-white p-4 rounded-2xl shadow">
         {dataKenaikanPangkat && (
           <LineChartCustom
