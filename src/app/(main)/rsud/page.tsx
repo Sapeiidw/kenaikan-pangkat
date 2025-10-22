96d57a240bdee664518be4b0acd86b4e88b2b243
Sapeiidw <sapeiidw@gmail.com>
2025-10-22 23:27:22 +0800

    add : table status pegawai

--- src/app/(main)/rsud/page.tsx
+++ src/app/(main)/rsud/page.tsx
@@ -3,9 +3,12 @@
 import BarChartCustom from "@/components/chart/BarChart";
 import LineChartCustom from "@/components/chart/LineChart";
 import PieChartCustom from "@/components/chart/PieChart";
+import { DataTableColumnHeader } from "@/components/data-table/column-header";
+import { DataTable } from "@/components/data-table/data-table";
 import { MonthPicker } from "@/components/month-picker";
 import { YearPicker } from "@/components/year-picker";
 import { useQuery } from "@tanstack/react-query";
+import { ColumnDef } from "@tanstack/react-table";
 import { useState } from "react";
 
 export default function Page() {
@@ -35,6 +38,65 @@ export default function Page() {
         `/api/status-pegawai?for=dashboard&id_opd=3&year=${year}`
       ).then((res) => res.json()),
   });
+
+  const columns: ColumnDef<unknown>[] = [
+    {
+      accessorKey: "tahun",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Tahun" />
+      ),
+      cell: ({ row }) => {
+        return new Date(row.getValue("periode")).getFullYear();
+      },
+    },
+    {
+      accessorKey: "periode",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Bulan" />
+      ),
+      cell: ({ row }) => {
+        return new Date(row.getValue("periode")).toLocaleDateString("id-ID", {
+          month: "long",
+        });
+      },
+    },
+    {
+      accessorKey: "nama_opd",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Nama OPD" />
+      ),
+    },
+    {
+      accessorKey: "nama",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Nama" />
+      ),
+    },
+    {
+      accessorKey: "nip",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="NIP" />
+      ),
+    },
+    {
+      accessorKey: "golongan",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Golongan" />
+      ),
+    },
+    {
+      accessorKey: "status",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Status" />
+      ),
+    },
+    {
+      accessorKey: "keterangan",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Keterangan" />
+      ),
+    },
+  ];
 
   const { data: dataStatusKenaikanPangkat } = useQuery({
     queryKey: ["status-kenaikan-pangkat", year],
@@ -104,6 +166,12 @@ export default function Page() {
           data={dataStatusSKKenaikanPangkat ?? []}
           field="value"
         />
+      </div>
+      <div className="w-full col-span-full flex flex-col bg-white p-4 rounded-2xl shadow">
+        <h1>Status Pegawai</h1>
+        {dataStatusPegawai && (
+          <DataTable columns={columns} data={dataStatusPegawai} />
+        )}
       </div>
     </>
   );
